33537f80ab7530fca4f43d73351fa058476042de
AltyIbrhm <87036697+AltyIbrhm@users.noreply.github.com>
2024-02-20 19:07:40 +0000

    Todos project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue,SetInputValue] = useState("")
+	const [todos,SetTodos] = useState([])
+
+		//Add into array ->concat
+		//Delete from array ->filter
+		//Update -> map
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		
+		<div className="container">
+			<h1>My Todos</h1>
+			<ul>
+				<li>
+					<input type="text" 
+					onChange={(e)=>SetInputValue(e.target.value)}
+					value={inputValue}
+
+					onKeyPress={(e)=>{
+						if(e.key === "Enter"){
+							SetTodos(todos.concat(inputValue));
+							SetInputValue("")
+						}
+					}}
+					placeholder="What do you need to do ?" />
+					</li>
+						
+						{todos.map((item,index)=>(
+							<li>
+							{item}{" "} <i class="fas fa-trash-alt"
+							onClick={()=>
+							SetTodos(todos.filter(
+								(t,currentIndex)=>
+								index != currentIndex
+							))
+							}
+							></i>
+							</li>
+						))}
+			
+			</ul>
+			<div>{todos.length}</div>
 		</div>
 	);
 };
